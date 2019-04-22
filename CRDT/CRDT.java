198df076a401221f5d5d82a3250cf66a5051be87
Hagai Raja Sinulingga <hagairaja@gmail.com>
2019-04-22 13:51:37 +0700

    adding skeleton for CRDT

---
+++ CRDT/CRDT.java
@@ -0,0 +1,24 @@
+public class CRDT {
+    List<Data> allData;
+    List<Integer> IDs;
+
+    public CRDT () {
+        allData = new ArrayList<Data>();
+    }
+
+    public void insert (char data, Data left, Data right) {
+        
+    }
+
+    public void delete (Data data) {
+
+    }
+
+    public int addUser () {
+
+    }
+
+    public void changeUserPos () {
+
+    }
+}
---
+++ CRDT/Data.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Data {
+    char alphabet;
+    int ID;
+    List<Integer> position;
+
+    public Data (char alphabet, int ID, List<Integer> position) {
+        this.alphabet = alphabet;
+        this.ID = ID;
+        this.position = position;
+    }
+}
