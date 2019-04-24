13ce84ba7c6e43b7beaf0ddbc66b6eedd6d6f566
leecopper15 <ntnicholasthie@gmail.com>
2019-04-24 15:24:52 +0700

    Added simple websocket client

--- src/Client/src/Main.java
+++ src/Client/src/Main.java
@@ -1,6 +1,28 @@
+import java.net.URI;
+
 public class Main {
-	public static void main(String args[])
-	{
+	public static void main(String args[]) throws Exception {
 		Editor e = new Editor();
+		
+		// Init client websocket
+		String clientPort = args[0];
+//    	String peer1Port = args[1];
+//    	String peer2Port = args[2];
+    	
+        final Messaging clientEndPoint = new Messaging(new URI("ws://localhost:" + clientPort + "/WebSocketProject/endpoint"));
+//        clientEndPoint.addMessageHandler(new Messaging.MessageHandler() {
+//            public void handleMessage(String message) {
+//            	clientEndPoint.sendMessage(message);
+//            }
+//        });
+//        final Messaging peer1EndPoint = new Messaging(new URI("ws://localhost:" + peer1Port + "/WebSocketProject/endpoint"));
+//        final Messaging peer2EndPoint = new Messaging(new URI("ws://localhost:" + peer2Port + "/WebSocketProject/endpoint"));
+ 
+        while (true) {
+            clientEndPoint.sendMessage("Hi to myself!! (" + clientPort + ")");
+//            peer1EndPoint.sendMessage("Hi to peer1 from " + clientPort + "!! (" + peer1Port + ")");
+//            peer2EndPoint.sendMessage("Hi to peer2 from " + clientPort + "!! (" + peer2Port + ")");
+            Thread.sleep(5000);
+        }
 	}
 }
